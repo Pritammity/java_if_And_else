1eb4ad2f01f9ebbfabc49d1a16b89f625d3d7b80
pritammity <maitypritam375@gmail.com>
2022-12-11 11:15:14 -0800

    test

---
+++ Java_if_and_else.java
@@ -0,0 +1,12 @@
+public class Java_if_and_else {
+    public static void main(String[] args) {
+        int a=20,b=30;
+        if(a+11==b){
+           System.out.println("ok");
+        }else if(a+12==b){
+            System.out.println("+ok");
+        }else{
+            System.out.println("-ok");
+        }
+    }
+}
